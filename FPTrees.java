5fe6223bd4f9fc5e9541b0d041173f69e119048d
wisard1862 <danieswain@gmail.com>
2015-04-14 10:24:09 -0400

    Added purpose comments

---
+++ FPTrees.java
@@ -0,0 +1,11 @@
+/**
+ * The purpose of this program is to replicate the Frequent Pattern Trees algorithm
+ * to generate the combinantions of highest frequency in the given data set.
+ *
+ * @author Daniel Swain Jr
+ **/
+
+
+public class FPTrees {
+
+}
